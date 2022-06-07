646f58af56a40a3444c2222e5a9af79780357e04
ruicc <kousuke.ruichi@herp.co.jp>
2022-06-07 16:56:45 +0900

    Move find-netrc to lib

---
+++ lib/find-netrc.nix
@@ -0,0 +1,16 @@
+{ lib }:
+netrc-file:
+  let
+    add-msg = netrc: to-msg: { inherit netrc; msg = to-msg netrc; };
+    path-exists = set: set.netrc != "" && set.netrc != null && builtins.pathExists set.netrc;
+    when-not-found = add-msg null (_: "netrc not found");
+    found = lib.findFirst
+      path-exists
+      when-not-found
+      [ (add-msg netrc-file (path: "netrc-file(${path}) found"))
+        (add-msg (builtins.getEnv "NETRC") (path: "NETRC(${path}) found"))
+        (add-msg (builtins.getEnv "PWD" + "/.netrc") (path: ".netrc(${path}) found"))
+        (add-msg (builtins.getEnv "HOME" + "/.netrc") (path: ".netrc(${path}) found"))
+        (add-msg "/etc/nix/netrc" (path: "netrc(${path}) found"))
+      ];
+  in builtins.trace found.msg found.netrc
